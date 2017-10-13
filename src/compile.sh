aa2044bfbf3e65316efdc0aa3fceac11292e525c
Adarsh <k.adarsh91@gmail.com>
2017-10-13 16:34:54 -0400

    Client registration is working

---
+++ src/compile.sh
@@ -0,0 +1,7 @@
+python3 -m da.compiler olympus.da
+python3 -m da.compiler replica.da
+python3 -m da.compiler classes.da
+python3 -m da.compiler constants.da
+python3 -m da.compiler logger.da
+python3 -m da.compiler client.da
+python3 -m da.compiler main.da
